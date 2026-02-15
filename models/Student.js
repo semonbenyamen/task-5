ecec9a813528fddba50ec538f6445cc831b97224
semonbenyamen <semobenyamen@gmail.com>
2026-02-15 14:50:50 +0200

    Create Student.js

---
+++ models/Student.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const StudentSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+      type: String,
+       required: true,
+       unique: true,
+       lowercase: true,
+    },
+  age: {
+      type: Number,
+      min: [5],
+  },
+  classroom: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Classroom",
+  }, { timestamps: true });
+
+module.exports  = mongoose.model("Student", StudentSchema);
+
